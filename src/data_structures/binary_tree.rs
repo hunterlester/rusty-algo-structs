9c1797292274e14bcb26c73559ba95037e205118
Hunter Lester <guilfordhunter@gmail.com>
2020-02-28 22:02:50 -0800

    update BT impl and adds tests

--- src/data_structures/binary_tree.rs
+++ src/data_structures/binary_tree.rs
@@ -18,45 +18,43 @@ impl BinaryTreeNode {
     }
 
     fn insert_left(&mut self, value: Option<i32>) -> () {
-        if let Some(outer_option_value) = self.left.as_ref() {
-            if let Some(node_value) = outer_option_value {
-                match self.right.as_ref() {
-                    Some(Some(_v)) => {
-                      node_value.borrow_mut().insert(value);
-                    },
-                    Some(None) => {
-                      self.insert_right(value)
-                    },
-                    None => {
-                      node_value.borrow_mut().insert(value);
-                    },
+        match self.left.as_ref() {
+            Some(Some(node)) => {
+                if let Some(None) = self.right.as_ref() {
+                    self.insert_right(value)
+                } else {
+                    node.borrow_mut().insert(value);
                 }
-            } else {
+           },
+            Some(None) => {
                 if let Some(integer) = value {
                   self.left = Some(Some(Rc::new(RefCell::new(BinaryTreeNode::new(integer)))));
                 } else {
                   self.left = None;
                 }
-            }
-        } else {
-            self.insert_right(value);
+            },
+            None => {
+                self.insert_right(value);
+            },
         }
     }
 
     fn insert_right(&mut self, value: Option<i32>) -> () {
-          if let Some(outer_option_value) = self.right.as_ref() {
-              if let Some(node_value) = outer_option_value {
-                  node_value.borrow_mut().insert(value);
+        match self.right.as_ref() {
+          Some(Some(node)) => {
+              node.borrow_mut().insert(value);
+          },
+          Some(None) => {
+              if let Some(integer) = value {
+                self.right = Some(Some(Rc::new(RefCell::new(BinaryTreeNode::new(integer)))));
               } else {
-                  if let Some(integer) = value {
-                    self.right = Some(Some(Rc::new(RefCell::new(BinaryTreeNode::new(integer)))));
-                  } else {
-                    self.right = None;
-                  }
+                self.right = None;
               }
-          } else {
-            self.insert_left(value);
-          }
+          },
+          None => {
+              self.insert_left(value);
+          },
+        }
     }
 
     pub fn insert(&mut self, value: Option<i32>) -> () {
@@ -69,5 +67,141 @@ impl BinaryTreeNode {
         } else {
           self.insert_left(value);
         }
+    }
+
+    fn inorder_traverse_left(&self) -> Vec<i32> {
+        if let Some(Some(node)) = self.left.as_ref() {
+            node.borrow().inorder_traverse()
+        } else {
+            vec![]
+        }
+    }
+
+    fn inorder_traverse_right(&self) -> Vec<i32> {
+        if let Some(Some(node)) = self.right.as_ref() {
+            node.borrow().inorder_traverse()
+        } else {
+            vec![]
+        }
+    }
+
+    pub fn inorder_traverse(&self) -> Vec<i32> {
+        vec![self.inorder_traverse_left(), vec![self.value], self.inorder_traverse_right()].concat()
+    }
+
+    fn preorder_traverse_left(&self) -> Vec<i32> {
+        if let Some(Some(node)) = self.left.as_ref() {
+            node.borrow().preorder_traverse()
+        } else {
+            vec![]
+        }
+    }
+
+    fn preorder_traverse_right(&self) -> Vec<i32> {
+        if let Some(Some(node)) = self.right.as_ref() {
+            node.borrow().preorder_traverse()
+        } else {
+            vec![]
+        }
+    }
+
+    pub fn preorder_traverse(&self) -> Vec<i32> {
+        vec![vec![self.value], self.preorder_traverse_left(), self.preorder_traverse_right()].concat()
+    }
+
+    fn postorder_traverse_left(&self) -> Vec<i32> {
+        if let Some(Some(node)) = self.left.as_ref() {
+            node.borrow().postorder_traverse()
+        } else {
+            vec![]
+        }
+    }
+
+    fn postorder_traverse_right(&self) -> Vec<i32> {
+        if let Some(Some(node)) = self.right.as_ref() {
+            node.borrow().postorder_traverse()
+        } else {
+            vec![]
+        }
+    }
+
+    pub fn postorder_traverse(&self) -> Vec<i32> {
+        vec![self.postorder_traverse_left(), self.postorder_traverse_right(), vec![self.value]].concat()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{BinaryTreeNode};
+
+    #[test]
+    fn new() {
+        let bt = BinaryTreeNode::new(1);
+        assert_eq!(bt.value, 1);
+        if let None = bt.left {
+            assert!(true);
+        }
+        if let None = bt.right {
+            assert!(true);
+        }
+    }
+
+    #[test]
+    fn insert() {
+        let mut bt = BinaryTreeNode::new(1);
+        bt.insert(Some(2));
+        if let Some(Some(node)) = bt.right {
+            assert_eq!(node.borrow().value, 2);
+        }
+    }
+
+    #[test]
+    fn insert_two() {
+        let mut bt = BinaryTreeNode::new(10);
+        assert_eq!(bt.value, 10);
+        bt.insert(Some(9));
+        bt.insert(Some(7));
+        bt.insert(Some(19));
+        if let Some(Some(node)) = bt.left.as_ref() {
+            assert_eq!(node.borrow().value, 9);
+        }
+        if let Some(Some(node)) = bt.right.as_ref() {
+            assert_eq!(node.borrow().value, 7);
+        }
+        if let Some(Some(node)) = bt.left.as_ref() {
+            if let Some(Some(left_node)) = node.borrow().left.as_ref() {
+                assert_eq!(left_node.borrow().value, 19);
+            }
+        }
+    }
+
+    #[test]
+    fn inorder_traverse() {
+        let mut bt = BinaryTreeNode::new(10);
+        bt.insert(Some(29));
+        bt.insert(Some(9));
+        bt.insert(Some(7));
+        bt.insert(Some(19));
+        assert_eq!(bt.inorder_traverse(), vec![7, 29, 19, 10, 9]);
+    }
+
+    #[test]
+    fn preorder_traverse() {
+        let mut bt = BinaryTreeNode::new(10);
+        bt.insert(Some(29));
+        bt.insert(Some(9));
+        bt.insert(Some(7));
+        bt.insert(Some(19));
+        assert_eq!(bt.preorder_traverse(), vec![10, 29, 7, 19, 9]);
+    }
+
+    #[test]
+    fn postorder_traverse() {
+        let mut bt = BinaryTreeNode::new(10);
+        bt.insert(Some(29));
+        bt.insert(Some(9));
+        bt.insert(Some(7));
+        bt.insert(Some(19));
+        assert_eq!(bt.postorder_traverse(), vec![7, 19, 29, 9, 10]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
 mod data_structures;
 
-use data_structures::BinaryTreeNode;
-
 fn main() {
-    let mut node_tree = BinaryTreeNode::new(1);
-    node_tree.insert(None);
-    // node_tree.insert(None);
-
-    node_tree.insert(Some(7));
-    node_tree.insert(Some(8));
-    println!("{:?}", node_tree);
 }
