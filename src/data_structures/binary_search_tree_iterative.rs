5fe39b956e4723a20d2e51fdd71e86cb08ee4fa2
hunterlester <guilfordhunter@gmail.com>
2020-05-04 10:15:22 -0700

    Iterative BST insertion

--- src/data_structures/binary_search_tree_iterative.rs
+++ src/data_structures/binary_search_tree_iterative.rs
@@ -12,99 +12,34 @@ pub struct IterativeBinarySearchTree {
 }
 
 impl IterativeBinarySearchTree {
-    pub fn new(value: i32) -> Self {
-        IterativeBinarySearchTree {
+    pub fn new(value: i32) -> PotentialNode {
+        Some(Rc::new(RefCell::new(IterativeBinarySearchTree {
             value,
             left: None,
             right: None,
             count: 1,
-        }
+        })))
     }
 
-    // pub fn insert_iterate(&mut self, value: i32) -> () {
-    //     let mut node_index = Rc::new(RefCell::new(*self));
-    //     println!("node_index: {:?}", &node_index);
-    //     let mut insert_left: Option<Rc<RefCell<IterativeBinarySearchTree>>> = None;
-    //     let mut insert_right: Option<Rc<RefCell<IterativeBinarySearchTree>>> = None;
-    //     let mut increment_count: bool = false;
-
-    //     loop {
-    //         let next_node: Rc<RefCell<IterativeBinarySearchTree>>;
-    //         if let Some(node) = insert_left.as_ref() {
-    //             println!("makes it here, insert_left: {:?}", &insert_left);
-    //             node_index.borrow_mut().left = insert_left;
-    //             println!("self: {:?}", self);
-    //             println!("node_index: {:?}", &node_index);
-    //             break;
-    //         } else if let Some(node) = insert_right.as_ref() {
-    //             node_index.borrow_mut().right = insert_right;
-    //             break;
-    //         } else if increment_count {
-    //             node_index.borrow_mut().count += 1;
-    //             break;
-    //         }
-    //         if value == node_index.borrow().value {
-    //             increment_count = true;
-    //             continue;
-    //         } else if value < node_index.borrow().value {
-    //             match node_index.borrow().left.as_ref() {
-    //                 Some(node) => {
-    //                     println!("setting next node");
-    //                     next_node = Rc::clone(&node);
-    //                 },
-    //                 None => {
-    //                     insert_left = Some(Rc::new(RefCell::new(IterativeBinarySearchTree::new(value))));
-    //                     continue;
-    //                 },
-    //             }
-    //         } else {
-    //             match node_index.borrow().right.as_ref() {
-    //                 Some(node) => {
-    //                     next_node = Rc::clone(&node);
-    //                 },
-    //                 None => {
-    //                     insert_right = Some(Rc::new(RefCell::new(IterativeBinarySearchTree::new(value))));
-    //                     continue;
-    //                 },
-    //             }
-    //         }
-    //         println!("next_node: {:?}", &next_node);
-    //         node_index = Rc::clone(&next_node);
-    //     }
-    // }
-
-
-    fn insert_left(&mut self, value: i32) -> () {
-        match self.left.as_ref() {
-            Some(node) => {
-                node.borrow_mut().insert(value);
-            },
-            None => {
-                self.left = Some(Rc::new(RefCell::new(IterativeBinarySearchTree::new(value))));
-            },
-        };
-    }
-
-    fn insert_right(&mut self, value: i32) -> () {
-        match self.right.as_ref() {
-            Some(node) => {
-                node.borrow_mut().insert(value);
-            },
-            None => {
-                self.right = Some(Rc::new(RefCell::new(IterativeBinarySearchTree::new(value))));
-            },
-        };
-    }
-
-    pub fn insert(&mut self, value: i32) -> () {
-        if value == self.value {
-            self.count += 1;
-        } else if value < self.value {
-            // traverse left
-            self.insert_left(value);
-        } else {
-            // traverse right
-            self.insert_right(value);
+    pub fn insert(root: Rc<RefCell<IterativeBinarySearchTree>>, value: i32) {
+        let mut current_node = Some(Rc::clone(&root));
+        while let Some(node) = current_node {
+            let mut borrowed_node = node.borrow_mut();
+            if value < borrowed_node.value {
+                if let Some(left_child) = borrowed_node.left.as_ref() {
+                    current_node = Some(Rc::clone(&left_child));
+                } else {
+                    borrowed_node.left = IterativeBinarySearchTree::new(value);
+                    current_node = None;
+                }
+            } else {
+                if let Some(right_child) = borrowed_node.right.as_ref() {
+                    current_node = Some(Rc::clone(&right_child));
+                } else {
+                    borrowed_node.right = IterativeBinarySearchTree::new(value);
+                    current_node = None;
+                }
+            }
         }
     }
 
@@ -196,59 +131,65 @@ impl IterativeBinarySearchTree {
 #[cfg(test)]
 mod tests {
     use super::IterativeBinarySearchTree;
+    use std::rc::Rc;
 
     #[test]
     fn inorder() {
         let mut bst = IterativeBinarySearchTree::new(10);
-        bst.insert(29);
-        bst.insert(9);
-        bst.insert(7);
-        bst.insert(19);
-        assert_eq!(bst.inorder(), vec![7, 9, 10, 19, 29]);
+        let values = vec![29, 9, 7, 19];
+        if let Some(tree) = bst.as_ref() {
+            for num in values {
+                IterativeBinarySearchTree::insert(Rc::clone(&tree), num);
+            }
+            assert_eq!(tree.borrow().inorder(), vec![7, 9, 10, 19, 29]);
+        }
     }
 
     #[test]
     fn preorder() {
         let mut bst = IterativeBinarySearchTree::new(10);
-        bst.insert(29);
-        bst.insert(9);
-        bst.insert(7);
-        bst.insert(19);
-        assert_eq!(bst.preorder(), vec![10, 9, 7, 29, 19]);
+        let values = vec![29, 9, 7, 19];
+        if let Some(tree) = bst {
+            for num in values {
+                IterativeBinarySearchTree::insert(Rc::clone(&tree), num);
+            }
+            assert_eq!(tree.borrow().preorder(), vec![10, 9, 7, 29, 19]);
+        }
     }
 
     #[test]
     fn postorder() {
         let mut bst = IterativeBinarySearchTree::new(10);
-        bst.insert(29);
-        bst.insert(9);
-        bst.insert(7);
-        bst.insert(19);
-        bst.insert(5);
-        bst.insert(8);
-        bst.insert(1);
-        assert_eq!(bst.postorder(), vec![1, 5, 8, 7, 9, 19, 29, 10]);
+        let values = vec![29, 9, 7, 19, 5, 8, 1];
+        if let Some(tree) = bst {
+            for num in values {
+                IterativeBinarySearchTree::insert(Rc::clone(&tree), num);
+            }
+            assert_eq!(tree.borrow().postorder(), vec![1, 5, 8, 7, 9, 19, 29, 10]);
+        }
     }
 
     // #[test]
     // fn level_order() {
     //     let mut bst = IterativeBinarySearchTree::new(10);
-    //     bst.insert(29);
-    //     bst.insert(9);
-    //     bst.insert(7);
-    //     bst.insert(19);
-    //     bst.insert(5);
-    //     bst.insert(8);
-    //     bst.insert(1);
-    //     assert_eq!(bst.bfs(), vec![10, 9 , 29, 7, 19, 5, 8, 1]);
+    //     let values = vec![29, 9, 7, 19, 5, 8, 1];
+    //     if let Some(tree) = bst {
+    //         for num in values {
+    //             IterativeBinarySearchTree::insert(Rc::clone(&tree), num);
+    //         }
+    //         assert_eq!(tree.borrow().bfs(), vec![10, 9 , 29, 7, 19, 5, 8, 1]);
+    //     }
     // }
 
-    // #[test]
-    // fn bst_insert_iteratively() {
-    //     let mut bst = IterativeBinarySearchTree::new(10);
-    //     bst.insert_iterate(9);
-    //     // bst.insert_iterate(7);
-    //     // bst.insert_iterate(19);
-    //     assert_eq!(bst.preorder(), vec![10, 9, 7, 19]);
-    // }
+    #[test]
+    fn bst_insert_iteratively() {
+        let mut bst = IterativeBinarySearchTree::new(10);
+        let values = vec![9, 7, 19];
+        if let Some(tree) = bst {
+            for num in values {
+                IterativeBinarySearchTree::insert(Rc::clone(&tree), num);
+            }
+            assert_eq!(tree.borrow().preorder(), vec![10, 9, 7, 19]);
+        }
+    }
 }
